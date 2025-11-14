3f1bfea89b12ce0c2c9f58f80e740db81aa41010
Mayuri <mayurisingh29122002@gmail.com>
2025-11-14 15:09:21 +0530

    deleted all the current code

--- urls.py
+++
@@ -1,8 +0,0 @@
-# accounts/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.login_signup_view, name='login_signup'),
-    path('logout/', views.logout_view, name='logout'),
-]
